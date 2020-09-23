3ee9d19f079c7da7061d10ffab3759b2a89602d1
Michael Zechmair <michael@zechmair.net>
2020-09-23 11:22:49 +0200

    Added Husky example

---
+++ examples/husky_braitenberg/cam_tf.py
@@ -0,0 +1,53 @@
+from NRPPythonModule import *
+import NRPGazeboDeviceInterfacePython
+from NRPNestPythonModule import NestDevice
+import numpy
+
+@SingleTransceiverDevice(keyword='camera', id=DeviceIdentifier('mouse_right_eye::camera', 'p_camera', 'gazebo'))
+@TransceiverFunction("nest")
+def transceiver_function(camera):
+    cam_width = int(round(camera.image_width))
+    cam_height = int(round(camera.image_height))
+    cam_depth = int(camera.image_depth)
+    eye_width = int(round(cam_width/2))
+
+    print("Camera Eye Width: " + str(eye_width))
+
+    no_r = 0.0
+    left_r  = 0.0
+    right_r = 0.0
+    for y in range(0, cam_height):
+        index_y = y*cam_width*camera.image_depth
+        for x in range(0, eye_width, cam_depth):
+            if camera.image_data[index_y+x+1] < 30 and camera.image_data[index_y+x+2] < 30:
+                left_r  = left_r+1
+            else:
+                no_r = no_r+1
+        for x in range(eye_width, cam_width, cam_depth):
+            if camera.image_data[index_y+x+1] < 30 and camera.image_data[index_y+x+2] < 30:
+                right_r  = right_r+1
+            else:
+                no_r = no_r+1
+
+    img_s = cam_height*cam_width
+    if img_s > 0:
+        left_r = 2.0 * left_r / img_s
+        right_r = 2.0 * right_r / img_s
+        no_r = no_r / img_s
+
+    print("Left Red:  " + str(left_r))
+    print("Right Red: " + str(right_r))
+    print("Go On:     " + str(no_r))
+
+    lpg = NestDevice(DeviceIdentifier("lpg", "nest_dev", "nest"))
+    lpg.data = {'rate': 2000.0*left_r}
+    rpg = NestDevice(DeviceIdentifier("rpg", "nest_dev", "nest"))
+    rpg.data = {'rate': 2000.0*right_r}
+
+    gpg = NestDevice(DeviceIdentifier("gpg", "nest_dev", "nest"))
+    gpg.data = {'rate': 75.0*no_r}
+   
+    print("lpg send: " + str(lpg.data))
+
+    return [ rpg, lpg, gpg ]
+
---
+++ examples/husky_braitenberg/mot_tf.py
@@ -0,0 +1,43 @@
+from NRPPythonModule import *
+from NRPGazeboDeviceInterfacePython import PhysicsJoint
+import NRPNestPythonModule
+
+@SingleTransceiverDevice(keyword='lwn', id=DeviceIdentifier('lwn', 'nest_dev', 'nest'))
+@SingleTransceiverDevice(keyword='rwn', id=DeviceIdentifier('rwn', 'nest_dev', 'nest'))
+@SingleTransceiverDevice(keyword='lpg', id=DeviceIdentifier('lpg', 'nest_dev', 'nest'))
+@TransceiverFunction("gazebo")
+def transceiver_function(lwn, rwn, lpg):
+    back_left_j   = PhysicsJoint("husky::back_left_joint")
+    back_right_j  = PhysicsJoint("husky::back_right_joint")
+    front_left_j  = PhysicsJoint("husky::front_left_joint")
+    front_right_j = PhysicsJoint("husky::front_right_joint")
+    
+    back_left_j.engine_name   = 'gazebo'
+    back_right_j.engine_name  = 'gazebo'
+    front_left_j.engine_name  = 'gazebo'
+    front_right_j.engine_name = 'gazebo'
+
+    left_voltage = lwn.data['E_L']
+    right_voltage = rwn.data['E_L']
+    
+    forward_vel = 1.0 * min(left_voltage, right_voltage)
+    rot_vel = 1.0 * (right_voltage - left_voltage)
+
+    print("lwn: " + str(lwn.data))
+    print("rwn: " + str(rwn.data))
+    print("lpg: " + str(lpg.data))
+
+    print("Left voltage:  " + str(left_voltage))
+    print("Right voltage: " + str(right_voltage))
+
+    print("Forward velocity: " + str(forward_vel))
+    print("Rotational vel:   " + str(rot_vel))
+
+    back_left_j.velocity = forward_vel - rot_vel
+    back_right_j.veclocity = -forward_vel - rot_vel
+
+    front_left_j.velocity = forward_vel - rot_vel
+    front_right_j.velocity = -forward_vel - rot_vel
+    
+    return [ back_left_j, back_right_j, front_left_j, front_right_j ]
+
