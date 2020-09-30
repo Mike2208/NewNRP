158463c9ebd94805c82d5775454d18c883cdc510
Michael Zechmair <michael@zechmair.net>
2020-09-30 20:26:42 +0200

    Renamed nrp_gazebo_device_interface to nrp_gazebo_json_engine and NRPGazeboDeviceInterface to NRPGazeboJSONEngine

---
+++ nrp_gazebo_json_engine/nrp_gazebo_json_engine/config/nrp_gazebo_cmake_constants.h
@@ -0,0 +1,44 @@
+#ifndef NRP_GAZEBO_CMAKE_CONSTANTS_H
+#define NRP_GAZEBO_CMAKE_CONSTANTS_H
+
+/* Automatically created by cmake configure file from nrp_gazebo_json_engine/config_headers/nrp_cmake_constants.h.in
+*  DO NOT EDIT DIRECTLY!
+*/
+
+/*!
+ * \brief Name of python module library
+ */
+#define GAZEBO_PYTHON_MODULE_NAME NRPGazeboJSONEnginePython
+
+/*!
+ * \brief Name of python module library as string
+ */
+#define GAZEBO_PYTHON_MODULE_NAME_STR "NRPGazeboJSONEnginePython"
+
+
+/*!
+ *	\brief Path to NRP gazebo plugins
+ */
+#define NRP_GAZEBO_PLUGINS_DIR "/usr/local/nrp/lib/nrp_gazebo_plugins"
+
+/*!
+ *	\brief Gazebo Communication Plugin Library Name
+ */
+#define NRP_GAZEBO_COMMUNICATION_PLUGIN "NRPGazeboCommunicationPlugin.so"
+
+/*!
+ *	\brief Path to default gazebo plugins
+ */
+#define DEFAULT_GAZEBO_PLUGIN_DIR "/usr/lib/gazebo-11/plugins"
+
+/*!
+ *	\brief Path to default gazebo models
+ */
+#define DEFAULT_GAZEBO_MODEL_DIR "/usr/share/gazebo-11/models"
+
+/*!
+ *	\brief Path to default gazebo libraries
+ */
+#define DEFAULT_GAZEBO_LIB_DIRS "/usr/lib:/usr/lib/gazebo-11/plugins"
+
+#endif // NRP_GAZEBO_CMAKE_CONSTANTS_H
---
+++ nrp_gazebo_json_engine/nrp_gazebo_json_engine/engine_server/gazebo_step_controller.cpp
@@ -0,0 +1,4 @@
+#include "nrp_gazebo_json_engine/engine_server/gazebo_step_controller.h"
+
+GazeboStepController::GazeboStepController() = default;
+GazeboStepController::~GazeboStepController() = default;
