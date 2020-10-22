185d3729d213ed70d17c94383b4ee4ebb172b5dd
Krzysztof Lebioda <krzysztof.lebioda@tum.de>
2020-10-22 21:50:53 +0200

    Create gRPC server instance in the constructor

--- nrp_general_library/nrp_general_library/engine_interfaces/engine_grpc_interface/engine_server/engine_grpc_server.cpp
+++ nrp_general_library/nrp_general_library/engine_interfaces/engine_grpc_interface/engine_server/engine_grpc_server.cpp
@@ -1,5 +1,6 @@
 #include <string>
 
+#include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
 
 #include "nrp_general_library/engine_interfaces/engine_grpc_interface/engine_server/engine_grpc_server.h"
@@ -9,4 +10,9 @@ EngineGrpcServer::EngineGrpcServer()
     std::string serverAddress("0.0.0.0:9002");
 
     grpc::EnableDefaultHealthCheckService(true);
+
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(serverAddress, grpc::InsecureServerCredentials());
+
+    _server = builder.BuildAndStart();
 }
--- nrp_general_library/nrp_general_library/engine_interfaces/engine_grpc_interface/engine_server/engine_grpc_server.h
+++ nrp_general_library/nrp_general_library/engine_interfaces/engine_grpc_interface/engine_server/engine_grpc_server.h
@@ -1,11 +1,17 @@
 #ifndef ENGINE_GRPC_SERVER_H
 #define ENGINE_GRPC_SERVER_H
 
+#include <grpcpp/grpcpp.h>
+
 class EngineGrpcServer
 {
     public:
 
         EngineGrpcServer();
+
+    private:
+
+    std::unique_ptr<grpc::Server> _server;
 };
 
 #endif // ENGINE_GRPC_SERVER_H
